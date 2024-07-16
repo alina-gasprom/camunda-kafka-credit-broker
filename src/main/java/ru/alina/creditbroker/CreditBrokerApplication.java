e63900086c7da8c9f6c2e997475b740666ecb1a0
alina-gasprom <alina.k.91@icloud.com>
2024-07-16 17:46:50 +0300

    start

---
+++ src/main/java/ru/alina/creditbroker/CreditBrokerApplication.java
@@ -0,0 +1,13 @@
+package ru.alina.creditbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditBrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditBrokerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alina/creditbroker/CreditBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alina.creditbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditBrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
